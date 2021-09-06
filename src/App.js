fb9aba4d3734148cc7d576607634854f1f9ea2a8
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2021-09-06 13:56:30 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import './App.css'
-import OldCalculator from './Components/OldCalculator'
 import DurationCalculator from './Components/DurationCalculator'
 
 function App () {
   return (
-    <>
+    <div>
       <h1 className='text-center'>Date Calculate</h1>
       <DurationCalculator />
-      {/* <OldCalculator /> */}
       <p className='text-center'>Created by Ayush </p>
-    </>
+    </div>
   )
 }
 
